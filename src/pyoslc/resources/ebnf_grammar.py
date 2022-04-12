238a33416932dbf25ae03ae55314dc20f5dabfbc
isccarrasco <mario.carrasco@gmail.com>
2022-04-11 23:10:28 -0500

    Update the EBNF grammar
    Updaring the grammar to parse the query parameters correctly when using more than one scoped terms
    
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- src/pyoslc/resources/ebnf_grammar.py
+++ src/pyoslc/resources/ebnf_grammar.py
@@ -2,18 +2,18 @@
 DIGIT = "0-9"
 LETTER = "A-Za-z"
 ALPHANUMERIC = LETTER + DIGIT
-DECIMAL = "[+-]?[" + DIGIT + "]+(\\.[" + DIGIT + "]+)?([eE][+-]?[" + DIGIT + "]+)?"
-STRING_ESC = "\".*?\""
-LANGTAG = "@[" + LETTER + "](-?[" + ALPHANUMERIC + "])*"
-PN_PREFIX = "[" + LETTER + "](\\.?[" + ALPHANUMERIC + "_-])*"
-PREFIXED_NAME = PN_PREFIX + ":[" + ALPHANUMERIC + "_](\\.?[" + ALPHANUMERIC + "_-])*"
-IDENTIFIER = "\\*|" + PREFIXED_NAME
-OPERATOR = "(([!<>]?=)|[<>])"
-IRI_REF = "<(([^<>\"{}\\|`\\\\])+)>"
-VALUE = "(" + IRI_REF + "|true|false|" + DECIMAL + "|" + STRING_ESC + "(" + LANGTAG + "|\\^\\^" + PREFIXED_NAME + ")?)"
-COMPARISION = OPERATOR + VALUE
-IN = " (in) \\[((.+?))\\]"
-SCOPED_TERM = "\\{((.+))\\}"
-TERM = r"(" + IDENTIFIER + ")((" + COMPARISION + ")|(" + IN + ")|(" + SCOPED_TERM + "))"
-PROPERTY = "(" + IDENTIFIER + ")((" + SCOPED_TERM + ")?)"
-PREFIX_DEF = "(" + PN_PREFIX + ")=" + IRI_REF
+DECIMAL = r"[+-]?[" + DIGIT + "]+(\.[" + DIGIT + "]+)?([eE][+-]?[" + DIGIT + "]+)?" #"[+-]?[" + DIGIT + "]+(\\.[" + DIGIT + "]+)?([eE][+-]?[" + DIGIT + "]+)?"
+STRING_ESC = r"\".*?\"" #"\".*?\"" 
+LANGTAG = r"@[" + LETTER + "](-?[" + ALPHANUMERIC + "])*" #"@[" + LETTER + "](-?[" + ALPHANUMERIC + "])*"
+PN_PREFIX = r"[" + LETTER + "](\.?[" + ALPHANUMERIC + "_-])*" # "[" + LETTER + "](\\.?[" + ALPHANUMERIC + "_-])*"
+PREFIXED_NAME = PN_PREFIX + r":[" + ALPHANUMERIC + "_](\.?[" + ALPHANUMERIC + "_-])*" # + " PN_PREFIX + ":[" + ALPHANUMERIC + "_](\\.?[" + ALPHANUMERIC + "_-])*"
+IDENTIFIER = r"\*|" + PREFIXED_NAME
+OPERATOR = r"(([!<>]?=)|[<>])"
+IRI_REF = r"<(([^<>\"{}\|`\\])+)>" #"<(([^<>\"{}\\|`\\\\])+)>"
+VALUE = r"(" + IRI_REF + "|true|false|" + DECIMAL + "|" + STRING_ESC + "(" + LANGTAG + "|\^\^" + PREFIXED_NAME + ")?)" #"(" + IRI_REF + "|true|false|" + DECIMAL + "|" + STRING_ESC + "(" + LANGTAG + "|\\^\\^" + PREFIXED_NAME + ")?)"
+COMPARISION = OPERATOR + VALUE # OPERATOR + VALUE
+IN = r" (in) \[((.+?))\]" # " (in) \\[((.+?))\\]"
+SCOPED_TERM = r"\{((.+?))\}" # "\\{((.+))\\}"
+TERM = r"(" + IDENTIFIER + ")((" + COMPARISION + ")|(" + IN + ")|(" + SCOPED_TERM + "))"   #"(" + IDENTIFIER + ")((" + COMPARISION + ")|(" + IN + ")|(" + SCOPED_TERM + "))"
+PROPERTY = r"(" + IDENTIFIER + ")((" + SCOPED_TERM + ")?)"
+PREFIX_DEF = r"(" + PN_PREFIX + ")=" + IRI_REF
