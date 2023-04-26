e37e46e7e8ead7fe9e8dc19f966e71820d33ddcc
isccarrasco <mario.carrasco@gmail.com>
2023-04-25 18:11:41 -0600

    Setting the version of RDFLib-JSONLD to work in python 2.7
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'python-dotenv',
         'RDFLib>=5.0.0',
-        'RDFLib-JSONLD',
+        'RDFLib-JSONLD==0.5.0',
         'Flask==1.0.2',
         'Flask-RESTx==0.5.1',
         'authlib~=0.14.3',
